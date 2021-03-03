d016bf00e931081a960f195fe9c09f51d95e484c
Guisoni <guisoni@yahoo.com.br>
2021-03-03 18:29:13 -0300

    feat: Added Start() and End() methods to Core class. Added a boolean member success.
    Start() calls SDL_Init() and End() calls SDL_Quit().

--- src/Core.cpp
+++ src/Core.cpp
@@ -2,6 +2,8 @@
 #define Core_H
 
 #include <iostream>
+
+#include "SDL2/SDL.h"
 class Core{
     public: 
      static Core *GetCore(){
@@ -16,17 +18,32 @@ class Core{
         std::cout<<"Core Deleted\n";
         delete core_;
     }
-    
+    void Start(){
+        if (SDL_Init(SDL_INIT_VIDEO) < 0) {
+            std::cerr << "SDL could not initialize.\n";
+            std::cerr << "SDL_Error: " << SDL_GetError() << "\n";
+            success_ = false;
+        }else{
+            std::cout << "SDL initialized.\n";
+        }
+    }
+    void End(){
+        std::cout << "Quit SDL.\n";
+        SDL_Quit();
+    }
+
     private:
     Core(){};
-    
-    static Core *core_; 
+    static Core *core_;
+    bool success_ = true;
 };
 #endif
 
-Core* Core::core_=nullptr;
+Core* Core::core_= nullptr;
 int main(){
     Core::GetCore();
     Core::FreeCore();
+    Core::GetCore()->Start();
+    Core::GetCore()->End();
 }
 
