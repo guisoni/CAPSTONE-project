6519c709160020c0882463de243264fb6ba3efda
Guisoni <guisoni@yahoo.com.br>
2021-03-03 23:33:39 -0300

    fix: Added SDL_DestroyTexture, SDL_DestroyRenderer, IMG_Quit()

--- src/Core.cpp
+++ src/Core.cpp
@@ -24,6 +24,11 @@ class Core{
             std::cerr << "SDL_Error: " << SDL_GetError() << "\n";
             success_ = false;
         }else{
+              //Set texture filtering to linear
+		      if( !SDL_SetHint( SDL_HINT_RENDER_SCALE_QUALITY, "1" ) )
+		      {
+			       std::cerr << "Warning: Linear texture filtering not enabled!\n";
+		    }
             std::cout << "SDL initialized.\n";
             // Create Window
             sdl_window_ = SDL_CreateWindow("Snake Game", SDL_WINDOWPOS_CENTERED,
@@ -44,9 +49,9 @@ class Core{
                 }else{
                     std::cout << "renderer allocated.\n";
 				    //Default renderer color
-				    SDL_SetRenderDrawColor( sdl_renderer_, 0xFF, 0xFF, 0xFF, 0xFF );
+				    SDL_SetRenderDrawColor( sdl_renderer_, 0, 0, 0, 0 );
 				    //Create PNG loading
-				    int flags = IMG_INIT_JPG|IMG_INIT_PNG;
+				    int flags = IMG_INIT_JPG;//|IMG_INIT_PNG;
 				    if( (IMG_Init( flags ) & flags)  != flags )
 				    {
                         std::cerr << "SDL_image could not initialize! SDL_image Error: " << IMG_GetError() << "\n"; 
@@ -57,22 +62,45 @@ class Core{
         }
     }    
     void End(){
+        if(texture_ != nullptr){
+            SDL_DestroyTexture( texture_ );
+            texture_ = nullptr;
+            std::cout << "Texture destroyed.\n";
+        }
+
+        if(sdl_renderer_ != nullptr){
+            SDL_DestroyRenderer( sdl_renderer_ );
+            sdl_renderer_ = nullptr;
+            std::cout << "Texture destroyed.\n";
+        }
+
+
         if(sdl_window_ != nullptr){
             SDL_DestroyWindow(sdl_window_);
             std::cout << "Window destroyed.\n";    
         }
+        std::cout << "Quit Image.\n";
+        IMG_Quit();
+
         std::cout << "Quit SDL.\n";
         SDL_Quit();
     }
     void Renderer(){
+        if(texture_ != nullptr){
+            SDL_DestroyTexture( texture_ );
+        texture_ = nullptr;
+        }
 	    //Load PNG image
-        std::string path = "../image/EARTH1.png";
+        std::string path = "../image/EARTH2.png";
 	    SDL_Surface* loadedSurface = IMG_Load( path.c_str() );
 	    if( loadedSurface == NULL )
 	    {
 		    std::cerr<< "Image " << path.c_str() << " not loaded! SDL_image Error: "<< IMG_GetError()<<"\n";
             success_ = false;
 	    }else{
+            //Set a color to be transparent
+		    SDL_SetColorKey( loadedSurface, SDL_TRUE, SDL_MapRGB( loadedSurface->format, 119, 236, 255 ) );
+
         	//Create texture from surface pixels
             texture_ = SDL_CreateTextureFromSurface( sdl_renderer_, loadedSurface );
 	        if( texture_ == nullptr )
@@ -85,8 +113,42 @@ class Core{
 		SDL_FreeSurface( loadedSurface );
         //Clear screen
 		SDL_RenderClear( sdl_renderer_ );
-		//Render texture to screen
-		SDL_RenderCopy( sdl_renderer_, texture_, NULL, NULL );
+        //Set rendering space and render to screen
+	    SDL_Rect imageQuad = { 0, 0, 600, 600 };
+        SDL_Rect renderQuad = { 0, 0, 300, 300 };
+        //Render texture to screen
+		SDL_RenderCopy( sdl_renderer_, texture_, NULL, &renderQuad );
+        
+        /*//Modulate color rgb
+           Uint8 red = 0, green = 65, blue = 30;
+	       if(SDL_SetTextureColorMod( texture_, red, green, blue ) < 0){
+               std::cerr<<"Color modulation not suported! SDL Error: " << SDL_GetError()<<"\n";
+           }else{
+               Uint8 *pred, *pgreen, *pblue; 
+               SDL_GetTextureColorMod( texture_, pred, pgreen, pblue );
+               std::cout<<"Modulation of: "<< *pred <<", "<< *pgreen <<", "<< *pblue <<", "<< "\n";
+           }*/
+
+           /*//Set blending function
+           SDL_BlendMode blendMode = SDL_BLENDMODE_BLEND; //NONE, ADD, BLEND, MOD
+	       if(SDL_SetTextureBlendMode( texture_, blendMode )<0){
+               std::cerr<<"Blending not suported! SDL Error: " << SDL_GetError()<<"\n";
+           }else{
+               SDL_BlendMode *pblendMode;
+               SDL_GetTextureBlendMode( texture_, pblendMode );
+               std::cout<<"Blend Mode: "<< *pblendMode <<", "<< "\n";
+           }
+	       //Modulate opacity (alpha)
+           Uint8 alpha = 100;
+	       if(SDL_SetTextureAlphaMod( texture_, alpha )<0){
+               std::cerr<<"AlphaMod not suported! SDL Error: " << SDL_GetError()<<"\n";
+           }else{
+               Uint8 *palpha;
+               SDL_GetTextureAlphaMod( texture_, palpha );
+               std::cout<<"Blend Mode: "<< *palpha <<", "<< "\n";
+           }*/
+
+
 		//Update screen
 		SDL_RenderPresent( sdl_renderer_ );
 	}
@@ -113,8 +175,8 @@ class Core{
     SDL_Window* sdl_window_ = nullptr;
     SDL_Renderer* sdl_renderer_ = nullptr;
     SDL_Texture* texture_= nullptr;
-    size_t screen_width{640};
-    size_t screen_height{640};
+    size_t screen_width{1820};
+    size_t screen_height{780};
     
 };
 #endif
