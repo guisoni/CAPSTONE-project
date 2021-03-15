91d164ab9fcf62bf36efe4fa3f022d6dde2b12a3
Guisoni <guisoni@yahoo.com.br>
2021-03-15 18:41:48 -0300

    refactor: removed files that did not belong here. Readme (copy).md and snake_CMakeLists.txt. Put const in arguments when possible in PixelMath namespace.

--- src/PixelMath.h
+++ src/PixelMath.h
@@ -1,9 +1,9 @@
 #ifndef PIXELMATH_H
 #define PIXELMATH_H
 #include <limits>
-#include "Physics.h"
+
 namespace PixelMath{
-    bool ConvertPositionToPixel(const Vector2Elm &position,SDL_Point &pixelposition, const SDL_Rect &source, const int screen_width, const int screen_height){
+    bool ConvertPositionToPixel(const Vector2Elm &position,SDL_Point &pixelposition, const SDL_Rect &source, const int &screen_width, const int &screen_height){
         long max_pos_x = std::numeric_limits<int>::max()-screen_width/2.0 + source.w/2.0;
         long max_pos_y = std::numeric_limits<int>::max() + screen_height/2.0 - source.h/2.0;
         if(AuxMath::abs(position.x_) <= max_pos_y && AuxMath::abs(position.y_) <= max_pos_y){  
